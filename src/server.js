0ed6b330eb71227ba294b1e7a2ba90950953e7a7
kawan santana <uchihakawan38@gmail.com>
2026-04-27 22:17:36 -0300

    criaçaõ do endpoint de listagem de usuarios.

--- src/server.js
+++ src/server.js
@@ -5,8 +5,21 @@ const app = express()
 function callbackDaRaiz( request, response ) {
     response.send("VAI CORINTHIANS!!!! 🦅")
 }
+function callbackDaRaizUsuarios(request, response) {
+    const listaUsuarios = [  
+         { 
+        id: 1, 
+        name: "kawan"
+    } 
+]
+
+response.json(listaUsuarios)
+}
 
 app.get("/", callbackDaRaiz)
+
+
+app.get("/users", callbackDaRaizUsuarios)
 
 app.listen(3000, () => {
     console.log("API rodando em http://localhost:3000")
